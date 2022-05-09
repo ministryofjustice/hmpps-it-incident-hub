cdbce7645aabbb633af280ede62ecaeb2ce43a6e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-05-09 16:44:53 +0100

    Rename app

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -9,7 +9,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Raise An Incident'
+  app.locals.applicationName = 'HMPPS IT Incident Hub'
 
   // Cachebusting version string
   if (production) {
