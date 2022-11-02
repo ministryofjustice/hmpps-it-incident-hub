f284d64d693f787a83f03b39f3f2ff146c645dbc
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-11-02 12:28:29 +0000

    DPSUP-396: Remove HMPPS from app name for display

--- logger.ts
+++ logger.ts
@@ -3,6 +3,6 @@ import bunyanFormat from 'bunyan-format'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
-const logger = bunyan.createLogger({ name: 'HMPPS IT Incident Hub', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({ name: 'IT Incident Hub', stream: formatOut, level: 'debug' })
 
 export default logger
