3b07988222f28f62871133277a143e49208cb286
DaveCritchlow <37904566+DaveCritchlow@users.noreply.github.com>
2023-02-12 23:04:55 +0000

    new-faq

--- server/data/faqData.ts
+++ server/data/faqData.ts
@@ -4,78 +4,63 @@ export default {
     faqs: [
       {
         id: 1,
-        heading: 'What is the visit someone in prison service',
-        body: 'The visit someone in prison service provides the ability for prisoners friends and relatives to book visiting times.',
+        heading: 'What is the visit someone in prison (VSiP) service?',
+        body: 'VSiP is a new service that is used to book, amend and cancel social face to face visits. It has been designed from the ground up to make the booking process as quick and seamless as possible. The service automatically takes into account things like prisoner non-associations, visitor restrictions and any locally specific booking schedules such as wing based booking or alternate week timetables. VSiP is accessed via the Digital Prison Service (DPS). VSiP will ultimately replace the visitor booking service in NOMIS.',
       },
       {
         id: 2,
-        heading: 'Is there a user guide for the visit someone in prison service?',
-        body: 'For guidance on how to use the service, you can view the user guide [here].',
+        heading: 'Which establishments use the service?',
+        body: 'The new service is being slowly rolled out across the prison estate. The initial focus is on establishments that manage their bookings through the Family Services contact centre, or establishments with significant operational challenges managing social visits.',
       },
       {
         id: 3,
-        heading: 'What are technical issues with the service?',
-        body: 'Technical issues are unintended problems with the service, such as slow loading pages and error messages presented by the service. You can report them via the ’raise an incident’ button below',
+        heading: 'What are the known technical issues with the service?',
+        body: 'There can be occasional slow page loads on the service. These are often related to the HMPPS Auth service that is used to verify your identity in the service. We are working with the HMPPS Auth team to improve how the two services work together. You do not need to report these issues.',
       },
       {
         id: 4,
-        heading: 'How do I provide feedback on the visit someone in prison service?',
-        body: 'You can send us feedback to help us to improve the service [here].',
+        heading: 'Why are some sessions not available?',
+        body: 'The dates and times of available sessions shown in the service are dependent on a few things:',
+      },
+      {
+        id: 5,
+        heading: 'How can I get access to the service?',
+        body: 'In order to access the service your establishment needs to have been onboarded. The VSiP team will work with you to understand your specific needs before we onboard your establishment. If you are interested in using the service to manage your establishments social face to face visits, you can complete this short form here and the team will get in touch with you: https://forms.office.com/e/K1ThmV7GmT ',
+      },
+      {
+        id: 6,
+        heading: 'How do I provide feedback on the service?',
+        body: 'If you have feedback, you can contact the team via the prisonvisitsbooking@digital.justice.gov.uk email address.',
       },
     ],
   },
-  manageincentives: {
-    clientName: 'Manage Incentives',
+  Pathfinder: {
+    clientName: 'Pathfinder',
     faqs: [
       {
         id: 1,
-        heading: 'What is the manage incentives service?',
-        body: 'Incentives is a data visualisation to support senior management at a local, regional and national level to use data in their decision-making so they can better deliver against the incentives framework .',
+        heading: 'What is the pathfinder service?',
+        body: '',
       },
       {
         id: 2,
-        heading: 'How do I access incentives?',
-        body: 'The service can be accessed via the ’Manage Incentives’ tile within the HMPPS Digital Services page. An active NOMIS account with caseload is required to access the service.',
+        heading: 'How do I access the pathfinder service?',
+        body: '',
       },
     ],
   },
-  mpc: {
-    clientName: 'Manage Prison Offender Managers’ cases',
+  onlinevisitrequest: {
+    clientName: 'Online Visit Requests',
     faqs: [
       {
         id: 1,
-        heading: 'What is the manage prison offender managers cases service?',
-        body: 'The manage prison offender managers’ cases service for POM and HOMDs which allows POMs to view their caseloads and manage their workload, whilst HOMDs can allocate cases to POMs and manage their staff.',
+        heading: 'What is the online visit request service?',
+        body: '',
       },
       {
         id: 2,
-        heading: 'How do I add staff members to start making allocations?',
-        body: 'Guidance for adding staff can be found at https://moic.service.justice.gov.uk/help_step0',
-      },
-      {
-        id: 3,
-        heading: 'I have quesitons about case responsibility?',
-        body: 'Guidance for case responsibility can be found at https://moic.service.justice.gov.uk/help/case_responsibility',
-      },
-    ],
-  },
-  restrictedpatients: {
-    clientName: 'Restricted Patients',
-    faqs: [
-      {
-        id: 1,
-        heading: 'What is the restricted patients service?',
-        body: 'This service is for managing offenders who are currently held in a secure hospital, but still under the responsibility of the prison. It allows users to transfer an offender to a hospital, remove them once they have been transferred back or released from hospital & also search for an offender to see if they are currently located within a hospital.',
-      },
-      {
-        id: 2,
-        heading: 'Offender is not showing in the restricted patients service?',
-        body: 'Check how the offender was booked out & into the restrcited patients service - was it using the new service or was the offender booked out via classic NOMIS. If the latter, then the prison need to book the offender back in, & then out again using the RP Service tool. ',
-      },
-      {
-        id: 3,
-        heading: 'Offenders have appeared on POM allocation service, yet are in a secure hospital?',
-        body: 'If the offender is still serving a prison sentence, then this is correct. The prison has a responsibility to manage a serving offender when they are in a secure hospital. They should therefore be allocated to a POM & managed accordingly.',
+        heading: 'How do I access the online visit requests service?',
+        body: '',
       },
     ],
   },
