80c838e8fd0b59c31d802ec7acc5dc92a3930078
DaveCritchlow <37904566+DaveCritchlow@users.noreply.github.com>
2023-04-13 12:29:14 +0100

    DPSUP-393-Update-FAQ

--- server/data/faqData.ts
+++ server/data/faqData.ts
@@ -1,36 +1,26 @@
 export default {
-  vsip: {
-    clientName: 'Visit Someone in Prison',
+  manageprisonvisits: {
+    clientName: 'Manage Prison Visits',
     faqs: [
       {
         id: 1,
-        heading: 'What is the visit someone in prison (VSiP) service?',
-        body: 'VSiP is a new service that is used to book, amend and cancel social face to face visits. It has been designed from the ground up to make the booking process as quick and seamless as possible. The service automatically takes into account things like prisoner non-associations, visitor restrictions and any locally specific booking schedules such as wing based booking or alternate week timetables. VSiP is accessed via the Digital Prison Service (DPS). VSiP will ultimately replace the visitor booking service in NOMIS.',
+        heading: 'What is the Manage prison visits service?',
+        body: 'The Manage prison visits service can be used to book, amend and cancel social face to face visits. It has been designed to make the booking process as quick and seamless as possible. The service automatically takes into account things like prisoner non-associations, visitor restrictions and any locally specific booking schedules. The service is accessed via the Digital Prison Service (DPS) and it will ultimately replace the visitor booking service in NOMIS.',
       },
       {
         id: 2,
-        heading: 'Which establishments use the service?',
-        body: 'The new service is being slowly rolled out across the prison estate. The initial focus is on establishments that manage their bookings through the Family Services contact centre, or establishments with significant operational challenges managing social visits.',
+        heading: 'What type of visits can i book?',
+        body: 'The service allows staff to book social face-to-face visits. We are working on supporting other visits types in the future (including official visits, Family Days and social video calls) but until then, these visits should be booked in NOMIS.',
       },
       {
         id: 3,
-        heading: 'What are the known technical issues with the service?',
-        body: 'There can be occasional slow page loads on the service. These are often related to the HMPPS Auth service that is used to verify your identity in the service. We are working with the HMPPS Auth team to improve how the two services work together. You do not need to report these issues.',
+        heading: 'Do I still need to do anything in NOMIS?',
+        body: '<p>Once your establishment has been onboarded it is vital that you do NOT make any more social face to face visit bookings or amendments in NOMIS. Any bookings made in NOMIS after the go live date will cause issues with overbooking as they are not synced back to the new service.</p><ul class="govuk-list govuk-list--bullet govuk-list--spaced"><li>All official visits and Family Days will still need to be booked, amended and cancelled in NOMIS.</li><li>All social video calls will still need to be booked, amended and cancelled in NOMIS.</li><li>Operational reports will still need to be created in NOMIS. All visits booked, amended and cancelled in the new service are automatically synced back to NOMIS so operational reports will be accurate. We are working on making operational reports available in the new service.</li><li>Establishments will need to continue to manage prisoner contacts in NOMIS, including adding or updating visitors and managing any visitor restrictions.</li><li>Establishments will need to continue to mark visitor attendance in NOMIS after visits have taken place. We are working on making attendance tracking available in the new service.</li><li>If your establishment uses a Biometric system, you will need to continue to download the visitor list from NOMIS.<li>',
       },
       {
         id: 4,
-        heading: 'Why are some sessions not available?',
-        body: '<p>The dates and times of available sessions shown in the service are dependent on a few things:</p><ul class="govuk-list govuk-list--bullet govuk-list--spaced"><li>There may be a non-association with another prisoner who is already booked into the session.</li><li>The prisoner might not have the required incentive level for that particular session.</li><li>The visitor might have a closed restriction which means they can only have closed visits and there are no closed slots available on that session.</li><li>The session has a local restriction (for example its only for a prisoners in a particular wing) and the prisoner does not meet the criteria.</li></ul><p>The service takes all these factors into account when presenting available sessions for a specific prisoner and their visitor(s).</p>',
-      },
-      {
-        id: 5,
         heading: 'How can I get access to the service?',
-        body: 'In order to access the service your establishment needs to have been onboarded. The VSiP team will work with you to understand your specific needs before we onboard your establishment. If you are interested in using the service to manage your establishments social face to face visits, you can complete this short <a href="https://forms.office.com/e/K1ThmV7GmT" class="govuk-link govuk-link--no-visited-state">form</a> and the team will get in touch with you.',
-      },
-      {
-        id: 6,
-        heading: 'How do I provide feedback on the service?',
-        body: 'If you have feedback, you can contact the team via the prisonvisitsbooking@digital.justice.gov.uk email address.',
+        body: 'In order to access the service your establishment needs to have been onboarded which is happening in phases. If you are interested in using the service to manage your establishments social face to face visits, you can complete this short <a href="https://forms.office.com/e/K1ThmV7GmT" class="govuk-link govuk-link--no-visited-state">form</a> and the team will get in touch with you.',
       },
     ],
     services: [
